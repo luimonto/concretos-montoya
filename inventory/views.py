ba96f797ec6b74e837c4eeec8116ee730ef5a3be
Luis Angel Montoya Navarro <montoya.n.luis.a@gmail.com>
2026-09-11 13:48:56 -0600

    detail machine view

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from inventory.models import Asset
+
+from inventory.models import Asset, AssetPhoto
+
 
 def detalle_maquina_qr(request, token):
-    maquina = get_object_or_404(Asset, qr_token=token)
-    
-    return render(request, 'maquinas/detalle.html', {'maquina': maquina})
+    maquina = get_object_or_404(
+        Asset.objects.select_related(
+            "category",
+            "equipment_type",
+            "brand",
+        ).prefetch_related("photos"),
+        qr_token=token,
+    )
+
+    foto_principal = maquina.photos.filter(
+        photo_type=AssetPhoto.PhotoType.GENERAL
+    ).first()
+
+    return render(
+        request,
+        "maquinas/detalle.html",
+        {
+            "maquina": maquina,
+            "foto_principal": foto_principal,
+        },
+    )
